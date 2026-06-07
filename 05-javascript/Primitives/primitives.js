21ada6afaa74c570f3df3fd8efdd4418b28f22de
Alan Saju <alansaju82@gmail.com>
2026-06-07 11:42:41 +0530

    Primitives in JS

---
+++ 05-javascript/Primitives/primitives.js
@@ -0,0 +1,53 @@
+// number
+
+let balance = 1000;
+
+let anotherBalance = new Number(1200);
+
+// console.log(balance);
+// console.log(anotherBalance);
+// console.log(anotherBalance.valueOf());
+
+// console.log(typeof(balance));
+// console.log(typeof(anotherBalance)); // It gives as object
+
+
+// Boolean
+
+// let isActive = true;
+// let isReallyActive = new Boolean(true); // Not recommended
+
+// console.log(typeof(isActive))
+// console.log(typeof(isReallyActive))
+
+
+
+// null and undefined
+
+// let firstName;
+// let lastName = null;
+// console.log(firstName);
+
+
+
+// string
+
+// let myString = "Hello";
+// let myStringOne = "Alan";
+
+// let oldGreet = myString + " " + "Alan";
+
+// console.log(oldGreet);
+
+// New way of system :
+// Using of backticks ``
+
+// String Interpolation 
+
+let greet = `Hello ${myStringOne}`;
+console.log(greet);
+
+// Symbol
+let sm1 = Symbol()
+let sm2 = Symbol ()
+

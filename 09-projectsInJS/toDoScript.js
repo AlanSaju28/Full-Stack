d26d3b12aebac178b9b01e54ca74ee7dff4edfd7
Alan Saju <alansaju82@gmail.com>
2026-06-13 10:37:21 +0530

    handling local storage and dom event

--- 09-projectsInJS/toDoScript.js
+++ 09-projectsInJS/toDoScript.js
@@ -1,21 +1,35 @@
-const todoInput = document.getElementById("todo-input");
-const addTaskButton = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded",()=>{
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
-let tasks = []; // storing tasks in an array
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  // let tasks = []; // storing tasks in an array
 
-addTaskButton.addEventListener("click", () => {
-  
-  const taskText = todoInput.value.trim(); // trim is used for spaces after the text is completed when user submits 
+  tasks.forEach(task => renderTask(task));
 
-  if (taskText === "") return; // this is for when empty is passed then it simply returns back
+  addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim(); // trim is used for spaces after the text is completed when user submits
 
-  const newTask = {
-    id: Date.now(),
-    text: taskText,
-    completed: false,
-  };
-  tasks.push(newTask);
-  todoInput.value = ""; // clear input
-  console.log(tasks);
-});
+    if (taskText === "") return; // this is for when empty is passed then it simply returns back
+
+    const newTask = {
+      id: Date.now(), // it generates a unique id everytime new task is created
+      text: taskText,
+      completed: false,
+    };
+    saveTasks();
+    tasks.push(newTask); // pushing the object newTask in to the array tasks
+    todoInput.value = ""; // clear input
+    console.log(tasks);
+  });
+
+  function renderTask(task) {
+    console.log(task);
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+})
